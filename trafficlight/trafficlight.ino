497333eac9694661737454ad4cf39b89e29ecc70
iansilbe <iansilbe@buffalo.edu>
2020-12-01 21:52:09 -0500

    Update 1

---
+++ trafficlight/trafficlight.ino
@@ -0,0 +1,17 @@
+int white = 13;
+int red = 12;
+int yel = 11;
+int green = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
