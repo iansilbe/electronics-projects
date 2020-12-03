5d375e1a6f02b4d8cf2fca123d88b4b6ac195bc7
iansilbe <iansilbe@buffalo.edu>
2020-12-02 21:57:22 -0500

    w/o red light camera

--- trafficlight/trafficlight.ino
+++ trafficlight/trafficlight.ino
@@ -5,13 +5,23 @@ int green = 10;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(10,OUTPUT);
+  pinMode(white,OUTPUT);
+  pinMode(red,OUTPUT);
+  pinMode(yel,OUTPUT);
+  pinMode(green,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  digitalWrite(red, HIGH);
+  digitalWrite(white, HIGH);
+  digitalWrite(green, LOW);
+  delay(5000);
+  digitalWrite(yel, HIGH);
+  digitalWrite(red, LOW);
+  digitalWrite(white, LOW);
+  delay(1000);
+  digitalWrite(green, HIGH);
+  digitalWrite(yel, LOW);
+  delay(4000);
 }
